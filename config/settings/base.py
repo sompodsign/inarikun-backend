379a8ce5cae43d453664aa8b8f63831a3f49a9a2
Shampad Sharkar <sompodsign@gmail.com>
2022-07-05 02:09:15 +0600

    create app named scraper

--- config/settings/base.py
+++ config/settings/base.py
@@ -82,6 +82,7 @@ LOCAL_APPS = [
     "inarikun.users",
     "core.apps.CoreConfig",
     "blog.apps.BlogConfig",
+    "scraper.apps.ScraperConfig",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
